7201ef0067bd5f3f1728bea82b502c23c9e816b8
estella <juju0448@naver.com>
2020-05-28 21:59:53 +0900

    changed the array size to 12_2

--- bubble.c
+++ bubble.c
@@ -15,7 +15,7 @@ int bubble_sort(int arr[], int n) {
 int main() {
     int arr[12] = {21,9,1,3,7,13,5,18,4,16,8,11};
     for (int i = 0; i < 12; i++)
-        printf("%d ", bubble_sort(arr,10) + i);
+        printf("%d ", bubble_sort(arr,12) + i);
 
     return 0;
 }
